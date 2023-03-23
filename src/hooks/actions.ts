10ec9113ac2d89c3d7de3f8fb964cfed1c056baf
Chibas <vesel22@gmail.com>
2023-03-23 09:56:01 +0000

    Deploying to gh-pages from  @ c52f6f67df7af387492a279aeb719e59676647dc 🚀

---
+++ src/hooks/actions.ts
@@ -0,0 +1,12 @@
+import { bindActionCreators } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import { theCatActions } from "../store/thecat/thecat.slice";
+
+const actions = {
+  ...theCatActions
+};
+
+export const useActions = () => {
+  const dispatch = useDispatch();
+   return bindActionCreators(actions, dispatch);
+}
