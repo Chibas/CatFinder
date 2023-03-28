0dd68a08d278e95447495216572747e347637080
Ihor Veselov <iveselov@360insights.com>
2023-03-28 13:49:41 -0400

    auth reducer and small fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation";
+import { useAuth } from "./hooks/useAuth";
 import FavoritesPage from "./pages/Favorites";
 import HomePage from "./pages/Home";
 
 function App() {
+  const { activeUser, isAuthenticated } = useAuth();
+
   return (
     <>
-      <Navigation />
+      <Navigation isAuthenticated={isAuthenticated} activeUser={activeUser} />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,7 +1,7 @@
 const FavoritesPage = () => {
   return (
-    <div className="container mx-auto py-10 w-screen">
-      <h3>WORK IN PROGRESS</h3>
+    <div className="container mx-auto py-10 w-screen text-center">
+      <h3 className="text-gray-600">No Images Found {"=("}</h3>
     </div>
   );
 };
